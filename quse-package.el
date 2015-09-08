db509ec66beb474dd3db1db4921ce8c9e7ad2faf
Jacob MacDonald <jaccarmac@gmail.com>
2015-09-08 12:36:40 -0600

    Fix indentation.

--- quse-package.el
+++ quse-package.el
@@ -4,7 +4,7 @@
 
 ;; Author: Jacob MacDonald <jaccarmac@gmail.com>.
 ;; Created: 18 December 2014.
-;; Version: 2.1.1
+;; Version: 2.1.3
 ;; Keywords: extensions lisp
 ;; Homepage: github.com/jaccarmac/quse-package
 ;; Package-Requires: ((quelpa "0") (use-package "0"))
@@ -19,15 +19,15 @@ use-package-form should be whatever comes after the package name in a
 use-package call. If the first element of use-package-form is :upgrade, the
 next element is used as the :upgrade parameter to the quelpa call."
   (let* ((upgrade-form (if (and use-package-forms
-                               (eq :upgrade (car use-package-forms)))
+                                (eq :upgrade (car use-package-forms)))
                            (list (car use-package-forms)
                                  (cadr use-package-forms))))
-        (use-package-name (if (listp quelpa-form)
-                              (car quelpa-form)
-                            quelpa-form))
-        (use-package-forms (if upgrade-form
-                               (cddr use-package-forms)
-                             use-package-forms)))
+         (use-package-name (if (listp quelpa-form)
+                               (car quelpa-form)
+                             quelpa-form))
+         (use-package-forms (if upgrade-form
+                                (cddr use-package-forms)
+                              use-package-forms)))
     `(progn (quelpa (quote ,quelpa-form) ,@upgrade-form)
             (use-package ,use-package-name
               ,@use-package-forms))))
